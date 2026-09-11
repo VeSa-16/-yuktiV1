689a4c650db7d9c2a4c3f7d9446ccdd2c6afd1e1
shrawandoshi-cyber <shrawandoshi@gmail.com>
2026-09-11 11:16:51 +0530

    fix: attach CARTO access token query param and required attributions

--- frontend/components/MapComponent.tsx
+++ frontend/components/MapComponent.tsx
@@ -39,8 +39,8 @@ export default function MapComponent({ lat, lng, radiusKm, showCompetitors, show
   return (
     <MapContainer center={[lat, lng]} zoom={12} style={{ height: "100%", width: "100%", backgroundColor: '#000000' }} scrollWheelZoom={false}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url={`https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=${process.env.NEXT_PUBLIC_CARTO_ACCESS_TOKEN || ''}`}
       />
       
       {/* Core Radius */}
